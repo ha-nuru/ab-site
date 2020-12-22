14f2d0e7beb4482507d329a777550797bc94c90a
ha-nuru <gkdk4195@gmail.com>
2020-12-22 14:29:50 +0900

    20.12.22

--- almond/assets/js/script.js
+++ almond/assets/js/script.js
@@ -4,12 +4,10 @@ tl.to($('.leaf'), 0.8, {
         ease:Linear.ease,
         opacity:1,
 });
-
-
 tl.to($('.prod_breez'), .3, {
         ease:Linear.ease,
         opacity: 1,
-    });
+});
 tl.to($('.drop_water'), .3, {
         opacity:1,
 });
@@ -40,3 +38,33 @@ var e1 = document.getElementsByClassName("flower2");
 var t2 = new TimelineLite()
 
 t2.staggerTo( [e0, e1], 1, {ease:Linear.ease, opacity: 1, right:-2+'%'});
+
+$(window).scroll(function(){
+let scroll_h = $(window).scrollTop();
+if(scroll_h >= $('#history').offset().top){
+        var t3 = new TimelineLite();
+        t3.to($('.hleft'), .8, {
+                ease:Linear.power2,
+                left:3+'%',
+                opacity: 1,
+        });
+        t3.to($('.hright_text'), .7, {
+                ease:Linear.ease,
+                opacity: 1,
+                right:0+'%'
+        });
+        t3.to($('.pack'), .7, {
+                ease:Linear.ease,
+                opacity: 1,
+                top:0+'%'
+        });
+     
+        var e0 = document.getElementsByClassName("p_al1");
+        var e1 = document.getElementsByClassName("p_al2");
+        var e2 = document.getElementsByClassName("p_al3");
+        var e3 = document.getElementsByClassName("p_al4");
+        var t4 = new TimelineLite()
+        t4.staggerTo( [e0, e1, e2, e3], 3, {top:50+"vw", repeat:-1, ease: Bounce.easeOut } ,0.5);
+       
+}
+})
